883b2ede6e4cd06a5a706df01a958a3b5ccd755b
lgrocha <luigrocha@gmail.com>
2021-03-16 13:29:27 -0500

    Passing arguments of a page to other page with PUSHNAMED

--- lib/src/widgets/movie_horizontal.dart
+++ lib/src/widgets/movie_horizontal.dart
@@ -35,7 +35,7 @@ class MovieHorizontal extends StatelessWidget {
   }
 
   Widget _tarjeta(BuildContext context, Pelicula pelicula) {
-    return Container(
+    final tarjeta = Container(
       margin: EdgeInsets.only(right: 15.0),
       child: Column(
         children: [
@@ -56,6 +56,14 @@ class MovieHorizontal extends StatelessWidget {
           )
         ],
       ),
+    );
+
+    return GestureDetector(
+      child: tarjeta,
+      onTap: () {
+        print('id: ${pelicula.title}');
+        Navigator.pushNamed(context, 'detalle', arguments: pelicula);
+      },
     );
   }
 
