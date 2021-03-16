9ace346299b0e79df9cead713e754c515fc25ac1
lgrocha <luigrocha@gmail.com>
2021-03-16 15:55:43 -0500

    Model to get actor and cast of movies

---
+++ lib/src/models/actores_model.dart
@@ -0,0 +1,66 @@
+class Cast {
+  List<Actor> actores = new List();
+
+  Cast.fromJsonList(List<dynamic> jsonList) {
+    if (jsonList == null) return;
+
+    jsonList.forEach((item) {
+      final actor = Actor.fromJsonMap(item);
+
+      actores.add(actor);
+    });
+  }
+}
+
+class Actor {
+  bool adult;
+  int gender;
+  int id;
+  String knownForDepartment;
+  String name;
+  String originalName;
+  double popularity;
+  dynamic profilePath;
+  int castId;
+  String character;
+  String creditId;
+  int order;
+  String department;
+  String job;
+
+  Actor({
+    this.adult,
+    this.gender,
+    this.id,
+    this.knownForDepartment,
+    this.name,
+    this.originalName,
+    this.popularity,
+    this.profilePath,
+    this.castId,
+    this.character,
+    this.creditId,
+    this.order,
+    this.department,
+    this.job,
+  });
+
+  Actor.fromJsonMap(Map<String, dynamic> json) {
+    castId = json['cast_id'];
+    id = json['id'];
+    name = json['name'];
+    adult = json['adult'];
+    order = json['order'];
+    gender = json['gender'];
+    profilePath = json['profile_path'];
+    creditId = json['credit_id'];
+  }
+
+  getFoto() {
+    if (profilePath == null) {
+      return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG1dXcbGQTvqLXLBEQnr3S2ta1flJNUUc6kw&usqp=CAU';
+    } else {
+      return 'https://image.tmdb.org/t/p/w500/$profilePath';
+    }
+  }
+}
