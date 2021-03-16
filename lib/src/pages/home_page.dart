1dea2ae3335d1d8ce24b930a5e3da6d6d8c93c82
lgrocha <luigrocha@gmail.com>
2021-03-16 11:35:20 -0500

    Subiendo curso flutter

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
+
+class HomePage extends StatelessWidget {
+  final peliculasProvider = new PeliculasProvider();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Peliculas en cine'),
+          backgroundColor: Colors.indigoAccent,
+          actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+        ),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[_swiperTarjetas(), _footer(context)],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData)
+          return CardSwiper(peliculas: snapshot.data);
+        else {
+          return Container(
+              height: 400.0, child: Center(child: CircularProgressIndicator()));
+        }
+      },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child: Text(
+              'Populares',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          FutureBuilder(
+            future: peliculasProvider.getPopulares(),
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              snapshot.data?.forEach((p) => print(p.title));
+              if (snapshot.hasData) {
+                return MovieHorizontal(peliculas: snapshot.data);
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:http/http.dart' as http;
+
+class PeliculasProvider {
+  String _apiKey = '7635d17a84ebefe5c36aaa3b16bf6b7c';
+  String _url = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
+  Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+
+    final peliculas = new Peliculas.fromJsonList(decodedData['results']);
+    return peliculas.items;
+  }
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language});
+    return await _procesarRespuesta(url);
+  }
+
+  Future<List<Pelicula>> getPopulares() async {
+    final url = Uri.https(
+        _url, '3/movie/popular', {'api_key': _apiKey, 'language': _language});
+    return await _procesarRespuesta(url);
+  }
+}
