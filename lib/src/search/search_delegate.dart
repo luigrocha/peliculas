3de56d423cc677945ef14554fb3ce28e5b2d4ea9
lgrocha <luigrocha@gmail.com>
2021-03-16 18:58:59 -0500

    Consuming api rest moviedb in the search component

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
 
 class DataSearch extends SearchDelegate {
   String seleccion = '';
+  final peliculasProvider = new PeliculasProvider();
   final peliculas = [
     'Spiderman',
     'Capitan America',
@@ -44,14 +47,44 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // crea los resultados que vamos a mostrar
-    return Center(
-      child: Container(
-        height: 100.0,
-        width: 100.0,
-        color: Colors.redAccent,
-        child: Text(seleccion),
-      ),
-    );
+    if (query.isEmpty) {
+      return Container();
+    }
+
+    return FutureBuilder(
+        future: peliculasProvider.buscarPelicula(query),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+          if (snapshot.hasData) {
+            final peliculas = snapshot.data;
+            return ListView(
+                children: peliculas.map((pelicula) {
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(pelicula.getPosterImg()),
+                  placeholder: AssetImage('assets/img/no-image.jpg'),
+                  width: 50.0,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: () {
+                  close(context, null);
+                  pelicula.uniqueId = '';
+                  Navigator.pushNamed(
+                    context,
+                    'detalle',
+                    arguments: pelicula,
+                  );
+                },
+              );
+            }).toList());
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        });
   }
 
   @override
